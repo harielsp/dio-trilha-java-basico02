7903e9afd9e218f9df1ef37efb8dff35ca475d4a
harielsp <harielsp@gmail.com>
2024-10-01 00:08:38 -0300

    encerrado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal  {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Pfv, digite o numero da agencia");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Pfv, digite o numero da agencia");
+        String agencia=scanner.next();
+
+        System.out.println("Digite o numero da conta");
+        int numero=scanner.nextInt();
+
+        System.out.println("Seu nome");
+        String nomeCliente=scanner.next();
+
+        System.out.println("Seu saldo");
+        double saldo=scanner.nextDouble();
+
+        System.out.println("Ola "+nomeCliente+", obrigado por cirar uma conta em nosso banco, sua agencia é " +agencia+", conta "+numero+" e seu saldo "+saldo +" 0ja está disponivel para saque");
+        
+        scanner.close();
     }
 }
